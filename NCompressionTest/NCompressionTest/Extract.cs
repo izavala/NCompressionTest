95ab8e67a7827830634693332f3fea4955e4e003
izavala <isaac.zavala@petabridge.com>
2019-05-03 13:31:50 -0500

    Takis in file name as command argument

---
+++ NCompressionTest/NCompressionTest/Extract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace NCompressionTest
+{
+    public class Extract
+    {
+        readonly string inFileName;
+        readonly string dataDirectoryA;
+
+        public Extract(string inputFileName)
+        {
+            inFileName = inputFileName;
+            dataDirectoryA = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(dataDirectoryA);
+            ZipFile.ExtractToDirectory(inFileName, dataDirectoryA);
+            Console.WriteLine("Writing Zip file to: ");
+            Console.WriteLine(dataDirectoryA);
+            ZipFile.CreateFromDirectory(dataDirectoryA, $"modified-{inputFileName}");
+        }
+    }
+}
---
+++ NCompressionTest/NCompressionTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NCompressionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var file = args[0];
+            var x =new Extract(file);
+            Console.ReadLine();
+        }
+    }
+}
